a4cd438431641911af9eaedd7ba30fb4799be7b3
Yuki Okita <yukiokita2018@gmail.com>
2025-07-21 05:14:00 +0900

    Update packages/cli/src/utils/userStartupWarnings.ts
    Co-authored-by: N. Taylor Mullen <ntaylormullen@google.com>

--- packages/cli/src/utils/userStartupWarnings.ts
+++ packages/cli/src/utils/userStartupWarnings.ts
@@ -38,7 +38,7 @@ const rootDirectoryCheck: WarningCheck = {
     try {
       const workspaceRealPath = await fs.realpath(workspaceRoot);
       const errorMessage =
-        'Warning: You are running Gemini CLI in the root directory. Your entire folder structure is being used for context. It is strongly recommended to run in a project-specific directory.';
+        'Warning: You are running Gemini CLI in the root directory. Your entire folder structure will be used for context. It is strongly recommended to run in a project-specific directory.';
 
       // Check for Unix root directory
       if (workspaceRealPath === '/') {
