4857a4716d8f77652dfa7b0d084ff19107bea804
Yuki Okita <ktewqp98r@gmail.com>
2025-08-02 00:41:48 +0900

    fix: make path from resolve to normalize

--- packages/cli/src/ui/commands/directoryCommand.tsx
+++ packages/cli/src/ui/commands/directoryCommand.tsx
@@ -19,7 +19,7 @@ export function expandHomeDir(p: string): string {
   } else if (p === '~' || p.startsWith('~/')) {
     expandedPath = os.homedir() + p.substring(1);
   }
-  return path.resolve(expandedPath);
+  return path.normalize(expandedPath);
 }
 
 export const directoryCommand: SlashCommand = {
